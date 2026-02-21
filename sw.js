8fb347690d88a21de018c5967691f5a8df93a0a9
RodriAi24 <130805316+RodriAi24@users.noreply.github.com>
2026-02-21 00:28:16 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE_NAME = 'gb-v8';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/', '/index.html']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
